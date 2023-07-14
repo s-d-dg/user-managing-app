3d67b4ae39399330f47c9bdfcf94da8b0d40ce42
Damian Gąsiorowski <softwaredevelopment.dg@gmail.com>
2023-07-14 15:28:19 +0200

    [UserManagingApp] - basic form validation

--- user-management/src/components/user-form-card/user-form/user-form.tsx
+++ user-management/src/components/user-form-card/user-form/user-form.tsx
@@ -2,7 +2,7 @@ import { CancelBtn } from "@/components/ui/buttons/cancel-form/cancel-btn";
 import { SubmitBtn } from "@/components/ui/buttons/submit/submit-btn";
 import { FormInput } from "@/components/ui/input/form-input";
 import { User } from "@/store/users/model";
-import { useForm, SubmitHandler } from "react-hook-form"
+import { useForm, SubmitHandler, FieldError } from "react-hook-form"
 import { styled } from "styled-components";
 
 interface Inputs {
@@ -23,6 +23,10 @@ const FormRow = styled.div`
     display: flex;
     `;
 
+const InputWithError = styled.div`
+    width: 100%
+`
+
 const ButtonsContainer = styled.div`
     margin-top: 30px;
     display: flex;
@@ -33,16 +37,36 @@ const CancelBtnContainer = styled(CancelBtn)`
     margin-right: 10px;
 `;
 
+const ErrorMessage = styled.div`
+    color: red;
+    font-size: 10px;
+`;
+
 export interface UserFormProps {
     user?: User;
     onCancel: () => void;
     onSubmit: (user: Omit<User, "id">) => void;
 }
 
+const getErrorMessage = (error: FieldError) => {
+    const type = error.type;
+    switch (type) {
+        case 'required':
+            return "This field is required";
+        case 'maxLength':
+            return "This value is too long"
+        case 'pattern':
+            return 'Incorrect format'
+    }
+}
+
 export default function UserForm({ user, onCancel, onSubmit }: UserFormProps) {
     const {
         register,
+        watch,
         handleSubmit,
+        getValues,
+        clearErrors,
         formState: { errors },
     } = useForm<Inputs>()
 
@@ -50,31 +74,59 @@ export default function UserForm({ user, onCancel, onSubmit }: UserFormProps) {
         onSubmit({ ...data });
     };
 
+    // console.log(getValues());
+
     return (
         <form onSubmit={handleSubmit(onSubmitForm)}>
 
             <FormRow>
                 <FormLabel>Name:</FormLabel>
-                <FormInput {...register("name", { value: user ? user.name : "", required: true })} />
-                {errors.name && <div>This field is required</div>}
+                <InputWithError>
+                    <FormInput {...register("name", {
+                        value: user ? user.name : "",
+                        required: true,
+                        maxLength: 30
+                    })} />
+                    {errors.name && <ErrorMessage>{getErrorMessage(errors.name)}</ErrorMessage>}
+                </InputWithError>
             </FormRow>
 
             <FormRow>
                 <FormLabel>Username:</FormLabel>
-                <FormInput {...register("username", { value: user ? user.username : "", required: true })} />
-                {errors.username && <div>This field is required</div>}
+                <InputWithError>
+                    <FormInput {...register("username", {
+                        value: user ? user.username : "",
+                        required: true,
+                        maxLength: 30
+                    })} />
+                    {errors.username && <ErrorMessage>{getErrorMessage(errors.username)}</ErrorMessage>}
+                </InputWithError>
             </FormRow>
+
 
             <FormRow>
                 <FormLabel>Email:</FormLabel>
-                <FormInput {...register("email", { value: user ? user.email : "", required: true })} />
-                {errors.email && <div>This field is required</div>}
+                <InputWithError>
+                    <FormInput {...register("email", {
+                        value: user ? user.email : "",
+                        required: true,
+                        maxLength: 50,
+                        pattern: /^\S+@\S+\.\S+$/
+                    })} />
+                    {errors.email && <ErrorMessage>{getErrorMessage(errors.email)}</ErrorMessage>}
+                </InputWithError>
             </FormRow>
 
             <FormRow>
                 <FormLabel>City:</FormLabel>
-                <FormInput {...register("city", { value: user ? user.city : "", required: true })} />
-                {errors.city && <div>This field is required</div>}
+                <InputWithError>
+                    <FormInput {...register("city", {
+                        value: user ? user.city : "",
+                        required: true,
+                        maxLength: 30
+                    })} />
+                    {errors.city && <ErrorMessage>{getErrorMessage(errors.city)}</ErrorMessage>}
+                </InputWithError>
             </FormRow>
 
             <ButtonsContainer>
